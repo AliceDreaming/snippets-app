3b71c3151201385708b1e71d6e22d96759f8cbf5
Alice Xie <alice.xie@autodesk.com>
2015-11-06 12:35:25 +0000

    Add get methos

--- snippets.py
+++ snippets.py
@@ -1,28 +1,30 @@
 import logging
 import argparse
 import sys
+import psycopg2
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
 def put(name, snippet):
-    """
-    Store a snippet with an associated name.
-
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    """Store a snippet with an associated name."""
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
     
 def get(name):
-    """Retrieve the snippet with a given name.
-
-    If there is no such snippet return an empty string
-
-    Returns the snippet.
-    """
-    logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
+    """Query a snippet with the sepecified name"""
+    logging.info("Querying snippet {!r}".format(name))
+    cursor = connection.cursor()
+    command = "select message from snippets where keyword=%s"
+    cursor.execute(command, (name,))
+    connection.commit()
+    logging.info("Query {!r} successfully".format(name))
+    return cursor.fetchone()
     
 def main():
     """Main function"""
@@ -35,6 +37,10 @@ def main():
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="The name of the snippet")
     put_parser.add_argument("snippet", help="The snippet text")
+    
+    logging.debug("Construct get subparser")
+    get_parser = subparsers.add_parser('get', help='Quesy a snippet')
+    get_parser.add_argument("name", help="The name of the snippet")
     
     arguments = parser.parse_args(sys.argv[1:])
     # Convert parsed arguments from Namespace to dictionary
@@ -49,4 +55,8 @@ def main():
         print("Retrieved snippet: {!r}".format(snippet))
 
 if __name__ == "__main__":
+    logging.debug("Connecting to PostgreSQL")
+    connection = psycopg2.connect("dbname='snippets'")
+    logging.debug("Database connection established.")
     main()
+    
